3da9746afacedb532e42fd55bfcd4b8a3410a393
alpatamerr <alpatamer@gmail.com>
2026-07-01 16:30:43 +0000

    Deployment of branch ri.branch..branch.038ab7de-84a3-4501-a21d-d034b9478e76 and deployment c42986a9-30c3-46df-a86b-b3ed2ed1342a

--- transforms-python/src/myproject/datasets/unified_fleet.py
+++ transforms-python/src/myproject/datasets/unified_fleet.py
@@ -8,10 +8,9 @@ import polars as pl
     airlabs=Input("ri.foundry.main.dataset.380832ab-a9ed-47f7-a5a9-c9df05e43938"),
     bikepoints=Input("/Atamer Systems-976c6b/Crisis Logistics Command System/02_clean_derived/tfl_bikepoints"),
     buses=Input("/Atamer Systems-976c6b/Crisis Logistics Command System/02_clean_derived/tfl_vehicle_positions"),
-    scooters=Input("/Atamer Systems-976c6b/Crisis Logistics Command System/02_clean_derived/lime_scooter_positions"),
     output=Output("ri.foundry.main.dataset.a774feec-e2a6-4f7e-8d14-279efe3f34ac"),
 )
-def compute(airlabs, bikepoints, buses, scooters, output):
+def compute(airlabs, bikepoints, buses, output):
     """Merge aircraft, bikepoint, and bus data into unified fleet view."""
     # Read airlabs aircraft data
     aircraft_df = airlabs.polars()
@@ -56,29 +55,8 @@ def compute(airlabs, bikepoints, buses, scooters, output):
             "last_seen_at": pl.Series([], dtype=pl.Utf8),
         })
 
-    # Read Lime scooter positions
-    scooters_df = scooters.polars()
-    if scooters_df.height > 0:
-        scooters_cols = scooters_df.select([
-            pl.col("unit_id"),
-            pl.col("vehicle_type"),
-            pl.col("latitude"),
-            pl.col("longitude"),
-            pl.col("unit_status").alias("status"),
-            pl.col("last_seen_at"),
-        ])
-    else:
-        scooters_cols = pl.DataFrame({
-            "unit_id": pl.Series([], dtype=pl.Utf8),
-            "vehicle_type": pl.Series([], dtype=pl.Utf8),
-            "latitude": pl.Series([], dtype=pl.Float64),
-            "longitude": pl.Series([], dtype=pl.Float64),
-            "status": pl.Series([], dtype=pl.Utf8),
-            "last_seen_at": pl.Series([], dtype=pl.Utf8),
-        })
-
     # Union all sources
-    unified = pl.concat([aircraft_cols, bikes_cols, buses_cols, scooters_cols], how="vertical_relaxed")
+    unified = pl.concat([aircraft_cols, bikes_cols, buses_cols], how="vertical_relaxed")
 
     # Filter to West London bounding box
     unified = unified.filter(
